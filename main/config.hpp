f9e99bcd46261ba6e29b2a814e63857654926f34
IGieckI <giacomo.antonelli3@gmail.com>
2026-06-09 15:14:54 +0200

    Implemented a configuration file

---
+++ main/config.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "driver/gpio.h"
+#include "driver/i2c.h"
+#include "hal/adc_types.h"
+
+// --- TDS Sensor Configuration ---
+#define TDS_ADC_UNIT        ADC_UNIT_1
+#define TDS_ADC_CHANNEL     ADC_CHANNEL_0
+
+// --- DS18B20 Temperature Sensor Configuration ---
+#define DS18B20_DATA_PIN    GPIO_NUM_32
+
+// --- BH1750 Light Sensor (I2C) Configuration ---
+#define I2C_MASTER_NUM      I2C_NUM_0
+#define I2C_MASTER_SDA_IO   21
+#define I2C_MASTER_SCL_IO   22
+#define I2C_MASTER_FREQ_HZ  400000
+
+// --- Main Loop Configuration ---
+#define LOOP_DELAY_MS       1000
